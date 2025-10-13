63cd154e9f4cc79f0007c0201a1d9f45068b8f8e
Gabriel <gabie00018@gmail.com>
2025-10-13 12:44:35 +0000

    enable multiline textbox

--- raygui.h
+++ raygui.h
@@ -2908,7 +2908,6 @@ int GuiTextBox(Rectangle bounds, char *text, int textSize, bool editMode)
     return result;      // Mouse button pressed: result = 1
 }
 
-/*
 // Text Box control with multiple lines and word-wrap
 // NOTE: This text-box is readonly, no editing supported by default
 bool GuiTextBoxMulti(Rectangle bounds, char *text, int textSize, bool editMode)
@@ -2928,7 +2927,6 @@ bool GuiTextBoxMulti(Rectangle bounds, char *text, int textSize, bool editMode)
 
     return pressed;
 }
-*/
 
 // Spinner control, returns selected value
 int GuiSpinner(Rectangle bounds, const char *text, int *value, int minValue, int maxValue, bool editMode)
